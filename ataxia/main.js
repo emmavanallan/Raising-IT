9e509290e2a660a9c808c5880d676c22dc607ac5
Kathie Wu <hellokathiewu@gmail.com>
2015-07-16 17:44:23 +0100

    Responsive, change Twitter text and Health Unlocked

--- ataxia/main.js
+++ ataxia/main.js
@@ -7,8 +7,15 @@ $(document).ready(function(){
 	$('.mainCallToAction ul.socialIcons').replaceWith('<ul class="socialIcons"><li class="helpline"><a href=""><i class="fa fa-envelope-o"></i> helpline@ataxia.org.uk</a></li><li class="phone"><a href=""><i class="fa fa-phone"></i> 0845 644 0606</a></li><li class="fbook"><a href=""><i class="fa fa-facebook"></i> Talk</a></li><li class="twitter"><a href=""><i class="fa fa-twitter"></i> Twitter</a></li></ul>');
 	
 	//make the carousel work better
-	var imgUrl = $('li.carouselSlide a.carouselSlideTitle img').attr('src');
-	$('li.carouselSlide a.carouselSlideTitle').css('background-image',  'url(' + imgUrl + ')');
+
+	$('a.carouselSlideTitle').each(function(){	
+		var imgUrl = $(this).find('img').attr('src');
+
+		$(this).css('background-image',  'url(' + imgUrl + ')');
+	});
+
+	// $('a.carouselSlideTitle').find('img').attr('')
+	// .css('background-image',  'url(' + imgUrl + ')');
 
 	//move the homepage intro to the right place
 	$('.homeIntroWrapper').insertAfter($('.homeFeaturesWrapper'));
@@ -26,4 +33,5 @@ $(document).ready(function(){
 	if ($("header.pageHeader").find(".adminBar").length > 0){ 
 	  $('.newsletter').hide();
 	}
+
 });
