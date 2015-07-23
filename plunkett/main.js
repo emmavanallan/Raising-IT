f2f0763bfc2c62478b88340bd40d4743afc81260
jigsawpieces <hellokathiewu@gmail.com>
2015-07-23 16:56:16 +0100

    Homepage done

--- plunkett/main.js
+++ plunkett/main.js
@@ -1,34 +1,32 @@
 $(document).ready(function(){
 
-	//put the nav menu in the right container
-	$('.menuMainAlt, nav.menuMain').prependTo($('.headerContent'));
-
 	//make the carousel work better
-
 	$('a.carouselSlideTitle').each(function(){	
 		var imgUrl = $(this).find('img').attr('src');
-
 		$(this).css('background-image',  'url(' + imgUrl + ')');
 	});
 
-	// $('a.carouselSlideTitle').find('img').attr('')
-	// .css('background-image',  'url(' + imgUrl + ')');
 
-	//move the homepage intro to the right place
-	$('.homeIntroWrapper').insertAfter($('.homeFeaturesWrapper'));
+	//make homepage features images work better
+	$('.homeFeature').each(function(){	
+		var imgUrl = $(this).find('img').attr('src');
+		$(this).css('background-image',  'url(' + imgUrl + ')');
+	});
 
-	//move the footer divs to the right container
-	$('.footerBox2, .NewsletterSign-up').prependTo($('.pageFooterWrapper'));
+
+	$('.homeFeature ').wrap('<div class="feature-wrapper"></div>');
 	
-	$('.footerBox1.NewsletterSign-up').wrap('<div class="newsletter-container"></div>');
+	//move the Newsletter up a level and wrap it in a container
+	$('.NewsletterSign-up').prependTo($('.pageFooterWrapper'));
+	$('.NewsletterSign-up').wrap('<div class="newsletter-container"></div>');
 	$('.groupSignUpBtn').wrap('<div class="formQuestion"></div>');
 
 	//contact page
-	$('body.contact article.post').wrap('<div class="pageWrapper"></div>');
+	// $('body.contact article.post').wrap('<div class="pageWrapper"></div>');
 
 	//if we are logged in, hide the newsletter sign up form
 	if ($("header.pageHeader").find(".adminBar").length > 0){ 
-	  $('.newsletter').hide();
+	  $('.newsletter-container').hide();
 	}
 
 });
